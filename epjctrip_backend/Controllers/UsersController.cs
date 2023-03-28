6534721aa982e8eb2386d0e905ada0a63d12c0ce
Joakim Myhre Wibe <71314249+JoakimWibe@users.noreply.github.com>
2023-03-28 12:40:11 +0200

    added password to database and created endpoint to log in

--- epjctrip_backend/Controllers/UsersController.cs
+++ epjctrip_backend/Controllers/UsersController.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using DefaultNamespace;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using epjctrip_backend.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace epjctrip_backend.Controllers
 {
@@ -14,110 +19,131 @@ namespace epjctrip_backend.Controllers
     public class UsersController : ControllerBase
     {
         private readonly TripContext _context;
+        private readonly IConfiguration _config;
 
-        public UsersController(TripContext context)
+        public UsersController(IConfiguration config, TripContext context)
         {
             _context = context;
+            _config = config;
         }
-
-        // GET: api/Users
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        
+        private string GenerateJwtToken(User user)
         {
-          if (_context.User == null)
-          {
-              return NotFound();
-          }
-            return await _context.User.ToListAsync();
-        }
-
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
-        {
-          if (_context.User == null)
-          {
-              return NotFound();
-          }
-            var user = await _context.User.FindAsync(id);
-
-            if (user == null)
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("JwtConfig:SecretKey").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
             {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
-        {
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
+                Subject = new ClaimsIdentity(new Claim[]
                 {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
-        // POST: api/Users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // // GET: api/Users
+        // [HttpGet]
+        // public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        // {
+        //   if (_context.User == null)
+        //   {
+        //       return NotFound();
+        //   }
+        //     return await _context.User.ToListAsync();
+        // }
+        //
+        // // GET: api/Users/5
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<User>> GetUser(int id)
+        // {
+        //   if (_context.User == null)
+        //   {
+        //       return NotFound();
+        //   }
+        //     var user = await _context.User.FindAsync(id);
+        //
+        //     if (user == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     return user;
+        // }
+
+        // // PUT: api/Users/5
+        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutUser(int id, User user)
+        // {
+        //     if (id != user.Id)
+        //     {
+        //         return BadRequest();
+        //     }
+        //
+        //     _context.Entry(user).State = EntityState.Modified;
+        //
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!UserExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+        //
+        //     return NoContent();
+        // }
+        
         [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
+        public async Task<ActionResult<User>> Login(LoginRequest login)
         {
-          if (_context.User == null)
-          {
-              return Problem("Entity set 'TripContext.User'  is null.");
-          }
-            _context.User.Add(user);
-            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
-        }
-
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(int id)
-        {
-            if (_context.User == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.User.FindAsync(id);
+            var user = _context.User.FirstOrDefaultAsync(user =>
+                user.Email == login.Email && user.Password == login.Password).Result;
+            
             if (user == null)
             {
-                return NotFound();
+                return Unauthorized();
             }
-
-            _context.User.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            
+            var token = GenerateJwtToken(user);
+            return Ok(new { token });
         }
 
-        private bool UserExists(int id)
-        {
-            return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        // // DELETE: api/Users/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteUser(int id)
+        // {
+        //     if (_context.User == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     var user = await _context.User.FindAsync(id);
+        //     if (user == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     _context.User.Remove(user);
+        //     await _context.SaveChangesAsync();
+        //
+        //     return NoContent();
+        // }
+        //
+        // private bool UserExists(int id)
+        // {
+        //     return (_context.User?.Any(e => e.Id == id)).GetValueOrDefault();
+        // }
     }
 }
