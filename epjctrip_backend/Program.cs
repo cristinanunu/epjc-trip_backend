65c36db41805ef9c400f84d9068c6ed62678da2b
Cristina Nunu <nunucristina0@gmail.com>
2023-04-28 16:27:30 +0200

    configured CORS policy

--- epjctrip_backend/Program.cs
+++ epjctrip_backend/Program.cs
@@ -1,8 +1,21 @@
 using epjctrip_backend.Repositories;
 using Microsoft.EntityFrameworkCore;
+
+var  myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TripContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TripContext") ?? throw new InvalidOperationException("Connection string 'TripContext' not found.")));
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000", "https://epjc-trip.netlify.app").AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 // Add services to the container.
 builder.Services.AddScoped<IPlanRepository, PlanRepository>();
@@ -24,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
